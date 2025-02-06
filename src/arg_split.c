b8c9d2e975a4db8a3d16b9d89db8b361cfad20c6
juaho <juaho@student.hive.fi>
2025-02-06 12:30:31 +0200

    Merge Again

--- src/arg_split.c
+++ src/arg_split.c
@@ -5,12 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: juaho <juaho@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/19 18:26:10 by juaho             #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/02/04 18:13:53 by juaho            ###   ########.fr       */
-=======
-/*   Updated: 2025/02/05 16:12:07 by juaho            ###   ########.fr       */
->>>>>>> pipex4
+/*   Created: 2025/02/06 12:26:13 by juaho             #+#    #+#             */
+/*   Updated: 2025/02/06 12:27:31 by juaho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +14,7 @@
 #include "../libft/libft.h"
 #include "../inc/pipex.h"
 
-<<<<<<< HEAD
-static int	whitespace_arg(char *arg);
-=======
 static int	nothing_but_whitespace(char *arg);
->>>>>>> pipex4
 static char	*format_arg(char *arg);
 static char	*find_matching_quote(char *arg);
 static void	erase_char(char *s);
@@ -32,39 +24,6 @@ char	**arg_split(char *arg)
 	char	*arg_formatted;
 	char	**av;
 
-<<<<<<< HEAD
-	if (whitespace_arg(arg))
-	{
-		av = (char **) ft_calloc(sizeof(char *), 2);
-		if (!av)
-			return (NULL);
-		av[0] = ft_strdup(arg);
-		if (!av[0])
-		{
-			free_av(&av);
-			return (NULL);
-		}
-		return (av);
-	}
-	argf = ft_strdup(arg);
-	if (!argf)
-		return (NULL);
-	argf = format_arg(argf);
-	av = ft_split(argf, -1);
-	free(argf);
-	return (av);
-}
-
-static int	whitespace_arg(char *arg)
-{
-	while (*arg)
-	{
-		if (!ft_isspace(*arg))
-			return (0);
-		arg++;
-	}
-	return (1);
-=======
 	if (!*arg || nothing_but_whitespace(arg))
 	{
 		av = ft_calloc(sizeof(char *), 2);
@@ -81,7 +40,6 @@ static int	whitespace_arg(char *arg)
 	av = ft_split(arg_formatted, -1);
 	free(arg_formatted);
 	return (av);
->>>>>>> pipex4
 }
 
 static int	nothing_but_whitespace(char *arg)
@@ -104,14 +62,6 @@ static char	*format_arg(char *arg_f)
 	i = 0;
 	while (arg_f[i])
 	{
-<<<<<<< HEAD
-		if (argf[i] == '\\' && ft_strchr(escapeable, argf[i + 1]))
-			ft_memmove(&argf[i], &argf[i + 1], ft_strlen(&argf[i]));
-		else if (argf[i] == '\'' && find_matching_quote(&argf[i]))
-			i += find_matching_quote(&argf[i]) - &argf[i];
-		else if (argf[i] == ' ')
-			argf[i] = -1;
-=======
 		if (arg_f[i] == '\\' && ft_strchr(escapeable, arg_f[i + 1]))
 			erase_char(&(arg_f[i]));
 		else if (arg_f[i] == '\'' && find_matching_quote(&arg_f[i]))
@@ -122,7 +72,6 @@ static char	*format_arg(char *arg_f)
 		}
 		else if (arg_f[i] == ' ')
 			arg_f[i] = -1;
->>>>>>> pipex4
 		i++;
 	}
 	return (arg_f);
